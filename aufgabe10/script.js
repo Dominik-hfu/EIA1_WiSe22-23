32ce8cf6b4f1fa4dc69417d6a5f0389b79e03b8b
Dominik-art-cpu <115712617+Dominik-hfu@users.noreply.github.com>
2022-12-15 20:02:46 +0100

    aufgabe10

--- aufgabe10/script.js
+++ aufgabe10/script.js
@@ -1,1 +1,25 @@
+let text = document.querySelector(".text");
+let todos = document.querySelector(".todos");
+text.addEventListener("keypress", function (ev) {
+    if (ev.key == "Enter" && text.value != "") {
+        const paragraph = document.createElement("p");
+        paragraph.innerText = text.value;
+        paragraph.classList.add("paragraph");
+        todos.prepend(paragraph);
+        const bin = document.createElement("p");
+        bin.classList.add("bin", "fa-solid", "fa-trash", "fa-2x");
+        bin.addEventListener("click", function () {
+            bin.classList.remove("paragraph");
+        });
+        paragraph.append(bin);
+        const circle = document.createElement("p");
+        circle.classList.add("circle", "fa-regular", "fa-circle", "fa-2x");
+        circle.addEventListener("click", function () {
+            circle.classList.remove("fa-regular", "fa-circle", "fa-2x");
+            circle.classList.add("fa-solid", "fa-check", "fa-2x");
+        });
+        paragraph.prepend(circle);
+        text.value = "";
+    }
+});
 //# sourceMappingURL=script.js.map
---
+++ aufgabe10/script.ts
@@ -0,0 +1,29 @@
+let text= document.querySelector(".text") as HTMLInputElement;
+let todos = document.querySelector(".todos") as HTMLDivElement;
+
+text.addEventListener("keypress", function(ev: KeyboardEvent){
+    if(ev.key=="Enter" && text.value != ""){
+
+    const paragraph = document.createElement("p");
+    paragraph.innerText = text.value;
+    paragraph.classList.add("paragraph")
+    todos.prepend(paragraph);
+
+    const bin = document.createElement("p");
+    bin.classList.add("bin", "fa-solid", "fa-trash", "fa-2x");
+    bin.addEventListener("click", function(){
+    bin.classList.remove ("paragraph");
+})
+    paragraph.append(bin);
+
+    const circle = document.createElement("p");
+    circle.classList.add("circle", "fa-regular", "fa-circle", "fa-2x");
+    circle.addEventListener("click", function(){
+        circle.classList.remove("fa-regular", "fa-circle", "fa-2x");
+        circle.classList.add("fa-solid", "fa-check", "fa-2x");
+    })
+
+    paragraph.prepend(circle);
+    text.value = "";
+}})
+
