af9ff1b688c8405f250c28bddb89b71a5fc4fd17
Dominik-art-cpu <115712617+Dominik-hfu@users.noreply.github.com>
2022-12-16 22:52:06 +0100

    10

--- aufgabe10/script.js
+++ aufgabe10/script.js
@@ -1,25 +1,42 @@
 let text = document.querySelector(".text");
 let todos = document.querySelector(".todos");
+// let termine = document.querySelector("do") as HTMLDivElement;
+let i = 0;
 text.addEventListener("keypress", function (ev) {
     if (ev.key == "Enter" && text.value != "") {
-        const paragraph = document.createElement("p");
-        paragraph.innerText = text.value;
-        paragraph.classList.add("paragraph");
-        todos.prepend(paragraph);
+        i += 1;
+        let anzahl = document.querySelector(".total");
+        anzahl.innerHTML = i + " in total";
+        const zeile = document.createElement("p");
+        zeile.innerText = text.value;
+        zeile.classList.add("paragraph", "do");
+        todos.prepend(zeile);
         const bin = document.createElement("p");
         bin.classList.add("bin", "fa-solid", "fa-trash", "fa-2x");
         bin.addEventListener("click", function () {
-            bin.classList.remove("paragraph");
+            i -= 1;
+            let anzahl = document.querySelector(".total");
+            anzahl.innerHTML = i + " in total";
+            circle.classList.remove("fa-regular", "fa-circle", "fa-2x", "circle");
+            bin.classList.remove("bin", "fa-solid", "fa-trash", "fa-2x");
+            todos.classList.remove("todos");
+            // termine.classList.remove("do", "paragraph", "todos");
+            // todos.classList.remove("todos");
         });
-        paragraph.append(bin);
+        zeile.append(bin);
         const circle = document.createElement("p");
         circle.classList.add("circle", "fa-regular", "fa-circle", "fa-2x");
         circle.addEventListener("click", function () {
             circle.classList.remove("fa-regular", "fa-circle", "fa-2x");
             circle.classList.add("fa-solid", "fa-check", "fa-2x");
         });
-        paragraph.prepend(circle);
+        zeile.prepend(circle);
         text.value = "";
     }
 });
+// text löschen
+// alle löschen
+// format behalten
+// stylen
+// evtl effekte beim löschen
 //# sourceMappingURL=script.js.map
